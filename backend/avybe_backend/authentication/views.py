427e9c4a67ca8c9b6c1ba10d405bd68bd9ec5817
savaseso <rigbence@gmail.com>
2021-03-02 17:00:03 -0800

    login functionality + jwt

--- backend/avybe_backend/authentication/views.py
+++ backend/avybe_backend/authentication/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer, LoginSerializer
 from rest_framework.response import Response
 from rest_framework import status
-# Create your views here.
+from django.conf import settings
+from django.contrib import auth
+import jwt
 
 
 class RegisterView(GenericAPIView):
@@ -14,5 +16,28 @@ class RegisterView(GenericAPIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status.HTTP_201_CREATED)
+            return Response(serializer.data, status.HTTP_201_CREATED)
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        data = request.data
+        username = data.get('username', '')
+        password = data.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+
+        if user:
+            auth_token = jwt.encode(
+                {'username': user.username}, 'secret-key')
+
+            serializer = UserSerializer(user)
+
+            data = {'user': serializer.data, 'token': auth_token}
+
+            return Response(data, status=status.HTTP_200_OK)
+
+            # SEND RES
+        return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
--- backend/avybe_backend/avybe_backend/settings.py
+++ backend/avybe_backend/avybe_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'authentication.jwtbackend.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,5 +124,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+#JWT
+JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+
 
 STATIC_URL = '/static/'
